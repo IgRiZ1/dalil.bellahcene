1c92cef4f92a3d35dfc78c6af6cec507acd3c1b7
IgRiZ1 <dalil.blh@outlook.be>
2025-08-15 18:15:11 +0200

    begin van het project maken

---
+++ src/test/java/ehb/be/dalilbellahcen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ehb.be.dalilbellahcen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
