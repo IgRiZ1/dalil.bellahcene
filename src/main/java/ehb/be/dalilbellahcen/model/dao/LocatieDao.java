73bdb8b019e677bbb66427c5e1220f429a6d88c6
IgRiZ1 <dalil.blh@outlook.be>
2025-08-16 11:39:44 +0200

    @repo

--- src/main/java/ehb/be/dalilbellahcen/model/dao/LocatieDao.java
+++ src/main/java/ehb/be/dalilbellahcen/model/dao/LocatieDao.java
@@ -2,6 +2,9 @@ package ehb.be.dalilbellahcen.model.dao;
 
 import ehb.be.dalilbellahcen.model.Locatie;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface LocatieDao extends CrudRepository<Locatie, Long> {
 }
--- src/main/java/ehb/be/dalilbellahcen/model/dao/eventDao.java
+++ src/main/java/ehb/be/dalilbellahcen/model/dao/eventDao.java
@@ -2,9 +2,10 @@ package ehb.be.dalilbellahcen.model.dao;
 
 import ehb.be.dalilbellahcen.model.Event;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface eventDao extends CrudRepository<Event, Long> {
     List<Event> findTop20ByOrderByTijdstipDesc();
 }
