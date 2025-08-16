fec517a1fb01c59aa11f548f6d7629c5de7a9dfa
IgRiZ1 <dalil.blh@outlook.be>
2025-08-16 11:41:48 +0200

    Update Application.java

--- src/main/java/ehb/be/dalilbellahcen/Application.java
+++ src/main/java/ehb/be/dalilbellahcen/Application.java
@@ -2,12 +2,13 @@ package ehb.be.dalilbellahcen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ehb.be.dalilbellahcen.model.dao")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
